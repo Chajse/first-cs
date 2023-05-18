030985b25e7f6e927d42322dec9ea2bff05b41fa
AJAAA <alloizeusjohndebelen1@gmail.com>
2023-05-19 01:58:45 +0800

    add c# string lesson

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// first c# with the boys
+
+//numero uno
+
+Console.WriteLine("Name: AJAAA");
+
+//numero dos
+
+Console.WriteLine("Name: "+ "AJAAA");
+
+//numero tres
+
+string myName = "AJAAA";
+Console.WriteLine("Name: " + myName);
+
+//numero quatro
+
+Console.WriteLine($"Name: {myName}");
+
+//numero singco
+
+string myFriend = "dominic molino";
+Console.WriteLine($"My name is {myName} and my megaidok's name is {myFriend}");
+
+//numero sais
+
+Console.WriteLine($"my name {myName} has {myName.Length} letters");
+Console.WriteLine($"my megaidok's name {myFriend} has {myFriend.Length} letters");
+
+//numero siyete
+
+string Thing = "Putok";
+Console.WriteLine($"Shawn has {Thing}");
